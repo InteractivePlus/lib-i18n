7963a87a0dcba83c667de01938e20335581bfbd0
Windy Feng <1745500559@qq.com>
2021-01-20 18:00:53 +0800

    Bugfix

--- src/Comparison.php
+++ src/Comparison.php
@@ -1,21 +1,21 @@
 <?php
 namespace LibI18N;
 class Comparison{
-    public static function bothEqualOrEmpty(string $left, string $right) : bool{
+    public static function bothEqualOrEmpty($left, $right) : bool{
         if($left === $right || empty($left) || empty($right)){
             return true;
         }else{
             return false;
         }
     }
-    public static function leftEqualOrEmpty(string $left, string $right) : bool{
+    public static function leftEqualOrEmpty($left, $right) : bool{
         if($left === $right || empty($left)){
             return true;
         }else{
             return false;
         }
     }
-    public static function rightEqualOrEmpty(string $left, string $right) : bool{
+    public static function rightEqualOrEmpty($left, $right) : bool{
         if($left === $right || empty($right)){
             return true;
         }else{
